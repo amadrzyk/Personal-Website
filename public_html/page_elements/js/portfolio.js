7cedcde6247ba3d60a4189fcf3946686104e7754
amadrzyk <alexmadrzyk@gmail.com>
2017-02-20 13:14:20 -0500

    Added material card design to portfolio items

--- public_html/page_elements/js/portfolio.js
+++ public_html/page_elements/js/portfolio.js
@@ -5,77 +5,53 @@ $("a").attr("target", "_blank");
 // DISCIPLINES
 $('.xelec').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.elec').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xdesi').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.desi').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xprog').click(function(){
     $('.pitem').addClass("hidden");
     $('.prog').removeClass("hidden");
-    $('.pitem').removeClass("pitema");
-    $('.switchrow').removeClass("row");
 });
 
 // LANGUAGES
 $('.xjava').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.java').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xpyth').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.pyth').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xwebdev').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.webdev').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xphp').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.php').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xclang').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.clang').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xmips').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.mips').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xswift').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.swift').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 $('.xadobe').click(function(){
     $('.pitem').addClass("hidden");
-    $('.pitem').removeClass("pitema");
     $('.adobe').removeClass("hidden");
-    $('.switchrow').removeClass("row");
 });
 
 
 // ALL
 $('.xall').click(function(){
     $('.pitem').removeClass("hidden");
-    $('.pitem').addClass("pitema");
-    $('.switchrow').addClass("row");
 });
